ad59da0904157de34b8a37f442bf192f3c6c7bdf
吴安琪 <764476876@qq.com>
2023-03-10 21:53:00 +0800

    项目初始demo

---
+++ src/test/java/com/angel/pluginmarket/PluginMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angel.pluginmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluginMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
